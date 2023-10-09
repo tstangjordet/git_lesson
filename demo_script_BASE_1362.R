e619e2e785dbf8e1841cef6c0e957fb9a9ff9c18
Thomas Bråten Stangjordet <thomas.satngjordet@student.nhh.no>
2023-10-09 22:57:57 +0200

    deleted unnecessary files

--- demo_script_BASE_1362.R
+++
@@ -1,8 +0,0 @@
-# Headline
-
-x <- rnorm(100)
-y <- rnorm(100)
-
-# Important feature
-
-# New feature
--- demo_script_LOCAL_1362.R
+++
@@ -1,8 +0,0 @@
-# Headline
-
-x <- rnorm(500)
-y <- rnorm(500)
-
-# Important feature
-
-# Normal development
